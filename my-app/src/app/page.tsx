1b1093817def2566eb49e5b984a01acac34c9be6
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-11-30 18:03:21 -0500

    everything works

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,25 +2,23 @@ import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
 import Buttons from "./components/Buttons";
 import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
 export default async function Home() {
   const session = await auth();
-  const loggedInUser = session?.user;
-  const username = loggedInUser?.name;
-  console.log(username)
+  const isLoggedIn = session?.user ? true : false;
 
+  const loggedInUser = session?.user.username;
   
   return (
     <div className="h-screen flex flex-row">
 
-        {username ? <img src="images/hnk.jpg" alt="picture"> </img> : <div className="w-1/2 bg-sky-600"> </div>}
+        {isLoggedIn ? <img className="w-1/2 object-cover" src="images/hnk.jpg" alt="picture" /> : <div className="w-1/2 bg-sky-600"> </div>}
 
-        <div className="px-5 w-1/2 flex flex-col justify-between">
-          <div className="flex flex-col gap-y-6">
-            <SignupForm/>
-            <LoginForm/>
-          </div>
-          
+        <div className="px-5 w-1/2 flex flex-col justify-between pt-5 ">
+
+          {isLoggedIn ? <h1 className="font-bold text-5xl">Welcome, {loggedInUser}.</h1> : <div className="flex flex-col gap-y-6"> <SignupForm/> <LoginForm/> </div>}
+
           <Buttons/>
         </div>
         
--- my-app/src/auth.config.ts
+++ my-app/src/auth.config.ts
@@ -5,4 +5,20 @@ export const authConfig: NextAuthConfig = {
         strategy: "jwt",
     },
     providers: [],
-};
+    callbacks: {
+        async jwt({ token, user }) {
+          if (user) {
+            token.id = user.id;
+            token.username = user.username;
+          }
+          return token;
+        },
+        async session({ session, token }) {
+          session.user = {
+            id: token.id,
+            username: token.username,
+          };
+          return session;
+        },
+      },
+    };
