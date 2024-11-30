99fe47097b4a70d7e3b70a28d383eaaa3ca433fa
Opuhlos <140357050+Opuhlos@users.noreply.github.com>
2024-11-30 15:44:13 -0500

    session

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,18 @@
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
+import { auth } from "@/auth";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const loggedInUser = session?.user;
+  const username = loggedInUser?.name;
+
+  
   return (
     <div className="h-screen flex flex-row">
-        <div className="w-1/2 bg-sky-600">
 
-        </div>
+        {username ? <img src="images/hnk.jpg" alt="picture"> </img> : <div className="w-1/2 bg-sky-600"> </div>}
 
         <div className="w-1/2 flex flex-col">
           <SignupForm/>
